4e5eb1804f6c9fd3ee21f6f01622aa5486805473
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 22:36:09 -0300

    repo de desafio

--- terraform/provider-azure.tf
+++ terraform/provider-azure.tf
@@ -1,15 +1,30 @@
 terraform {
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = ">= 4.4.0"
     }
   }
-
+  backend "azurerm" {
+    resource_group_name  = "rg-staticsite-lb-tf-william"
+    storage_account_name = "staticsitelbtfwilliam"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {}
-  alias = "cloud"
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
+
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-staticsite-lb-william"
+    location = "brazilsouth"
+}
+
+#####
